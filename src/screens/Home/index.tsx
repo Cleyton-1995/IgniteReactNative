ca12e9ce75ea286d16e61d9250036ad2451d6785
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-04 11:41:32 -0300

    feat: add to input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text style={styles.eventName} >
+        Nome do Evento
+      </Text>
+
+      <Text style={styles.eventDate} >
+        Terça-feira, 04 de abril de 2023.
+      </Text>
+
+      <TextInput style={styles.input} />
     </View>
   );
 }
