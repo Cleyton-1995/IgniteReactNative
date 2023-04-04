f1044e81ab0b21621a2771332032c57b6f466534
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-04 11:51:36 -0300

    feat: add properties to TextInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import React from "react";
+import { Text, TextInput, View } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName} >
-        Nome do Evento
-      </Text>
+      <Text style={styles.eventName}>Nome do Evento</Text>
 
-      <Text style={styles.eventDate} >
-        Terça-feira, 04 de abril de 2023.
-      </Text>
+      <Text style={styles.eventDate}>Terça-feira, 04 de abril de 2023.</Text>
 
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
     </View>
   );
 }
