29e961a944143aef6914c9ec0822c5142f1e9ec0
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-04 11:35:11 -0300

    feat: add styling to Home screen

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+
+  eventName: {
+    color: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48
+  },
+
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  }
 });
