97aeeb65832c702e7a512f520f1761c1d0860f35
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-04 12:01:30 -0300

    feat: add styling to button

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -26,5 +26,19 @@ export const styles = StyleSheet.create({
     color: "#FFFFFF",
     padding: 16,
     fontSize: 16,
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 24
+  },
+
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
