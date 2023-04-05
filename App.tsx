a81ff936878bcf975317a175b046de211056034b
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-05 11:54:55 -0300

    feat: statusbar customization

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens/Home';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home/>
-      <StatusBar style="auto" />
+      <Home />
+      <StatusBar translucent style="light" backgroundColor="transparent" />
     </View>
   );
 }
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });
