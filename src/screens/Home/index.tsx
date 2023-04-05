4b290a6bbcd2c9b8be06ddac3ce62f2369c00882
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-05 16:28:11 -0300

    feat: add onChangeText property to TextInput to store value in state

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -49,6 +49,7 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={text => setParticipantName(text)}
         />
 
         <TouchableOpacity
