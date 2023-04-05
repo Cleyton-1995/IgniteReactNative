91957d294e87417da8636241e835b7de48a33a0f
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2023-04-05 07:35:09 -0300

    refactor: wrapping the Text Input and Button in a View

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,9 +4,8 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
-
   function handlePaticipantAdd() {
-    console.log("Você clicou no botão de Adicionar!")
+    console.log("Você clicou no botão de Adicionar!");
   }
 
   return (
@@ -14,18 +13,22 @@ export function Home() {
       <Text style={styles.eventName}>Nome do Evento</Text>
 
       <Text style={styles.eventDate}>Terça-feira, 04 de abril de 2023.</Text>
+      
+      <View style={styles.form} >
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
-
-      <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={handlePaticipantAdd} >
-        <Text style={styles.buttonText} >
-          +
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.8}
+          onPress={handlePaticipantAdd}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
